3828c8e77668168ff775743aa6bb89423660ef91
leond <lad258@cornell.edu>
2018-01-04 13:22:25 -0500

    function to convert t1 coordinates to MNI coordinates

--- map_mni_coords.py
+++ map_mni_coords.py
@@ -1,6 +1,8 @@
 from nibabel import nifti1
 import numpy as np
-
+from .config import Paths
+import subprocess
+import os.path as osp
 
 def map_coords(coordinates,nifti_file):
     """
@@ -20,3 +22,43 @@ def map_coords(coordinates,nifti_file):
     ras_coords = coordinates * transform.astype(np.mat)
     return ras_coords[:3,:]
 
+
+def mni_from_t1_mri(t1_mri_coords,imaging_root,subject,name=''):
+    """
+    Uses the ANTS binaries identified in config.Paths to map T1 MRI coordinates into MNI space,
+    via a set of transformations specified by files located at imaging_root
+    :param t1_mri_coords {np.array}: An Nx3 array of MRI coordinates
+    :param imaging_root: The path to the imaging directory
+    :param subject:
+    :param name:
+    :return {np.array}: An Nx3 array of MNI coordinates
+    """
+    t1_mri_coords[:,:2] *= -1
+    antsApplyTransform = osp.join(Paths.ants_root,'antsApplyTransformsToPoints')
+    t1_to_itk = osp.join(imaging_root,'T01_CT_to_T00_mprageANTs0GenericAffine_RAS_itk.txt')
+    template_to_subject_affine = osp.join(imaging_root,'T00','thickness',
+                                          '{sub}TemplateToSubject0GenericAffine.mat'.format(sub=subject))
+    template_to_subject_warp  = osp.join(imaging_root,'T00','thickness',
+                                         '{sub}TemplateToSubject1InverseWarp.nii.gz'.format(sub=subject))
+    faffine = osp.join(osp.dirname(Paths.ants_root),'localization','template_to_NickOasis','ch22t0GenericAffine.mat')
+    finversewarp = osp.join(osp.dirname(faffine),'ch22t1InverseWarp.nii.gz')
+    if name:
+        name = name + '_'
+
+    mri_filename = osp.join(imaging_root,'electrode_coordinates_%st1.csv'%name)
+    with open(mri_filename,'w') as mri_coords_file:
+        mri_coords_file.write('x,y,z,t')
+        for line in t1_mri_coords:
+            mri_coords_file.write(','.join(['{:.4f}'.format(x) for x in line])+',0')
+    mni_filename = mri_filename.replace('t1','mni_from_t1')
+    subprocess.check_call([antsApplyTransform, '-d','3','-i',mri_filename,'-o',mni_filename,
+                     '-t','[%s,1]'%t1_to_itk,
+                    '-t', '[%s,1]'%template_to_subject_affine,
+                    '-t', template_to_subject_warp,
+                    '-t','[%s,1]'%faffine,
+                    '-t', finversewarp,
+                    '-t',t1_to_itk]
+                    )
+    corrected_mni_coordinates = np.loadtxt(mni_filename,delimiter=',',skiprows=1)
+    corrected_mni_coordinates[:,:2] *= -1
+    return corrected_mni_coordinates[:,:3]
